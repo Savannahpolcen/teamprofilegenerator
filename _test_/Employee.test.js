a8b4069b9044c624dd3178c8d55c928c10d5a668
Savannahpolcen <Savannahpolcen@gmail.com>
2022-04-25 00:41:15 -0400

    failed test for Employee.

---
+++ _test_/Employee.test.js
@@ -0,0 +1,38 @@
+const Employee = require('../lib/Employee.js')
+
+test ('creates Employee object', ()=>{
+    const Employee = new Employee('Sally');
+
+    expect(Employee.name).toBe('Sally');
+    expect(Employee.officenum).toEqual(expect.any(Number));
+    expect(Employee.id).toEqual(expectany(Number));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// object names need to be capitilized
+// Need, Name, Email, Github, employee id# and office number
+
+//const Employee = require ('../lib/Employee')
+
+//test('creates Employee object' , () => {
+   // const Employee = new Employee('Sal','sal55@salmail.com','salHub',1,17 );
+
+    //expect(Employee.name).toBe('Sal');
+    //expect(Employee.email).toEqual('sal55@salmail.com');
+    //expect(Employee.github).toEqual('salHub');
+    //expect(Employee.id).toEqual('1');
+   // expect(Employee.officenum).toEqual('17');
+//});
